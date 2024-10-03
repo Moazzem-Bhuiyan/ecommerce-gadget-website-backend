53e3d1d813f459d0add61b57ae4572e2bc9e62a2
Moazzem <tiyonbhuiyan.201@gmail.com>
2024-10-04 03:48:16 +0600

    Deploy

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// const uri = "mongodb+srv://gadgetweb:8E0f1vNWvJqJD4Ny@cluster0.7kns6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://gadgetweb:8E0f1vNWvJqJD4Ny@cluster0.7kns6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const uri = 'mongodb://localhost:27017'
+// const uri = 'mongodb://localhost:27017'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // collection type start----
 
@@ -106,8 +106,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
